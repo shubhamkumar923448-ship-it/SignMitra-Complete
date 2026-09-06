c16e2dcdc81b409d61a7cf2bc6333bdfee360fb5
Shubham <shubhamkumar923448@gmail.com>
2026-09-06 13:24:21 +0530

    Updating some file

--- Python/main.py
+++ Python/main.py
@@ -20,7 +20,7 @@ app = FastAPI(title="SignMitra AI Microservices", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], # Allows all origins for testing
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
